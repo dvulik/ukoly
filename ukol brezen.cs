be2b2e870ce54e92b7fbc7279fd4d3fe3f4bce56
dvulik <125461503+dvulik@users.noreply.github.com>
2023-03-15 13:03:13 +0100

    Add files via upload

---
+++ ukol brezen.cs
@@ -0,0 +1,24 @@
+﻿using Spectre.Console;
+
+string[] months = { "Leden", "Únor", "Březen", "Duben", "Květen", "Červen", "Červenec", "Srpen", "Září", "Říjen", "Listopad", "Prosinec" };
+var selectedMonth = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+        .Title("vyber si [blue]měsíc[/]")
+        .PageSize(12)
+        .AddChoices(months));
+
+int numberOfDays = DateTime.DaysInMonth(DateTime.Now.Year, Array.IndexOf(months, selectedMonth) + 1);
+int[] poleTeplot = new int[numberOfDays];
+
+for (int i = 0; i < numberOfDays; i++)
+{
+    Console.WriteLine("zadej teplotu pro " + (i + 1) + ". den");
+    int teplota = Convert.ToInt32(Console.ReadLine());
+    poleTeplot[i] = teplota;
+}
+
+double average = Queryable.Average(poleTeplot.AsQueryable());
+
+Console.WriteLine("Průměrná teplota: " + average + "°C" );
+Console.WriteLine("Minimální teplota: " + (poleTeplot.Min()) + "°C");
+Console.WriteLine("Maximální teplota: " + (poleTeplot.Max()) + "°C");
